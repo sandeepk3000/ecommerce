211b9640bc43696db2525f1dd7dd626da9e14717
sandeep kumar <geelucknow@gmail.com>
2023-10-22 09:29:52 -0700

    set dburl

--- config/db.js
+++ config/db.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 const connectToDB = () => {
-    const uri = "mongodb://127.0.0.1:27017/shoppingWeb"
-    // const uri = "mongodb+srv://codejourney7:zbDrFK7owhKF4RVI@shopping.orl4rzr.mongodb.net/skecommerce?retryWrites=true&w=majority";
-    return mongoose.connect(uri)
+    // const uri = "mongodb://127.0.0.1:27017/shoppingWeb"
+    const uri = "mongodb+srv://codejourney7:zbDrFK7owhKF4RVI@shopping.orl4rzr.mongodb.net/skecommerce?retryWrites=true&w=majority";
+    return mongoose.connect(uri,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
 
 }
 
 module.exports = connectToDB;
-// {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// }
