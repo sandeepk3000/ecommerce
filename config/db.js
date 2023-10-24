506f59185b9d71bd9740f36d2b9597cb17340794
sandeep kumar <geelucknow@gmail.com>
2023-10-24 02:09:18 -0700

    replace db url

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const connectToDB = () => {
-    const uri = "mongodb://127.0.0.1:27017/shoppingWeb"
+    // const uri = "mongodb://127.0.0.1:27017/shoppingWeb"
     const uri = "mongodb+srv://codejourney7:zbDrFK7owhKF4RVI@shopping.orl4rzr.mongodb.net/skecommerce?retryWrites=true&w=majority";
     return mongoose.connect(uri,{
         useNewUrlParser: true,
